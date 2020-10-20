4ffb1ace2f176fa2ee73ae5b7e3113d0c878616b
rafa <r.salguero@garsalabs.com>
2020-10-20 10:13:36 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const fs = require('fs');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const htmlContent = fs.readFileSync("./index.html").toString();
+
+const clientConfig = {
+    entry: ['./src/app/index.tsx', './style/index.css'],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                loader: "file-loader",
+                options: {
+                    name: '[path][name].[ext]?[contenthash]'
+                }
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css?[contenthash]',
+        }),
+        new HtmlWebpackPlugin({
+            templateContent: htmlContent,
+            minify: false
+        })
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+        modules: [
+            "node_modules",
+            __dirname
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'bundle.js?[contenthash]',
+        publicPath: "/"
+    }
+};
+
+module.exports = clientConfig;
